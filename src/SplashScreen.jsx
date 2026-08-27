2ef23f3bcc5f5789a60dc9c7cda164282dc886f5
pdxproduction02-lab <pdxproduction02@gmail.com>
2026-08-27 17:09:01 +0530

    Create SplashScreen.jsx

---
+++ src/SplashScreen.jsx
@@ -0,0 +1,11 @@
+import "./SplashScreen.css";
+
+export default function SplashScreen() {
+  return (
+    <div className="healthscope-splash">
+      <div className="scan-square">
+        <div className="scan-line"></div>
+      </div>
+    </div>
+  );
+}
